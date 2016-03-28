9d347fe66f8cb20c555a9e4163632d9eb3e5231e
Joss Whittle <joss.whittle@l2program.co.uk>
2016-03-28 23:44:34 +0100

    added debug macro

--- string_ext/string_ext.h
+++ string_ext/string_ext.h
@@ -1,83 +1,63 @@
 #pragma once
 
-#include <cstdio>
-#include <cstdarg>
+#include <iostream>
 #include <cctype>
 #include <string>
 #include <sstream>
 #include <iomanip>
-#include <functional>
-#include <utility>
-#include <type_traits>
 #include <cassert>
 
-struct _Format {
-	char specifier;
-	bool leftJustify;
-	bool forceSign;
-	bool forceLong;
-	bool padZeros;
-	unsigned int width;
-	unsigned int precision;
-	_Format() : specifier(0), 
-				leftJustify(false), forceSign(false), 
-				forceLong(false), padZeros(false), 
-				width(0), precision(0) {};
-};
+namespace string_ext {
+	struct _Format {
+		char specifier;
+		bool leftJustify;
+		bool forceSign;
+		bool forceLong;
+		bool padZeros;
+		unsigned int width;
+		unsigned int precision;
+		_Format() : specifier(0),
+			leftJustify(false), forceSign(false),
+			forceLong(false), padZeros(false),
+			width(0), precision(0) {};
+	};
 
-inline _Format _parseFormat(std::string::const_iterator &pos, std::string::const_iterator &fmtE) {
-	_Format fmt;
-	/// Assume pos starts on '%'
-	int mode = 0;
-	while ((++pos) != fmtE) {
-		if (mode == 0) {
-			/// Flags
-			if (*pos == '-') {
-				fmt.leftJustify = true;
-				//std::cout << "LEFTJUSTIFY: " << std::endl;
+	inline _Format _parseFormat(const int _line_, const char *_file_, std::string::const_iterator &pos, std::string::const_iterator &fmtE) {
+		_Format fmt;
+		/// Assume pos starts on '%'
+		int mode = 0;
+		while ((++pos) != fmtE) {
+			if (mode == 0) {
+				/// Flags
+				if (*pos == '-') {
+					fmt.leftJustify = true;
+					//std::cout << "LEFTJUSTIFY: " << std::endl;
+				}
+				else if (*pos == '+') {
+					fmt.forceSign = true;
+					//std::cout << "FORCESIGN: " << std::endl;
+				}
+				else if (*pos == '0') {
+					fmt.padZeros = true;
+					//std::cout << "PADZEROS: " << std::endl;
+				}
+				else if (*pos == '#') {
+					fmt.forceLong = true;
+					//std::cout << "FORCELONG: " << std::endl;
+				}
+				else {
+					pos--;
+					mode++;
+					continue;
+				}
 			}
-			else if (*pos == '+') {
-				fmt.forceSign = true;
-				//std::cout << "FORCESIGN: " << std::endl;
-			}
-			else if (*pos == '0') {
-				fmt.padZeros = true;
-				//std::cout << "PADZEROS: " << std::endl;
-			}
-			else if (*pos == '#') {
-				fmt.forceLong = true;
-				//std::cout << "FORCELONG: " << std::endl;
-			}
-			else {
-				pos--;
-				mode++;
-				continue;
-			}
-		}
-		else if (mode == 1) {
-			// Width
-			auto n = pos;
-			while (n != fmtE && std::isdigit(*n)) n++; 
-			if (n != pos) {
-				fmt.width = std::stoi(std::string(pos, n));
-				//std::cout << "WIDTH: " << fmt.width << std::endl;
-				pos = n - 1;
-			}
-			else {
-				pos--;
-			}
-			mode++;
-			continue;
-		}
-		else if (mode == 2) {
-			// Precision
-			if (*pos == '.') {
-				pos++;
+			else if (mode == 1) {
+				// Width
 				auto n = pos;
 				while (n != fmtE && std::isdigit(*n)) n++;
 				if (n != pos) {
-					fmt.precision = std::stoi(std::string(pos, n));
-					//std::cout << "PRECISION: " << fmt.precision << std::endl;
+					fmt.width = std::stoi(std::string(pos, n));
+					//std::cout << "WIDTH: " << fmt.width << std::endl;
 					pos = n - 1;
 				}
 				else {
@@ -86,143 +66,219 @@ inline _Format _parseFormat(std::string::const_iterator &pos, std::string::const
 				mode++;
 				continue;
 			}
-			else {
-				pos--;
-				mode++;
-				continue;
+			else if (mode == 2) {
+				// Precision
+				if (*pos == '.') {
+					pos++;
+					auto n = pos;
+					while (n != fmtE && std::isdigit(*n)) n++;
+					if (n != pos) {
+						fmt.precision = std::stoi(std::string(pos, n));
+						//std::cout << "PRECISION: " << fmt.precision << std::endl;
+						pos = n - 1;
+					}
+					else {
+						pos--;
+					}
+					mode++;
+					continue;
+				}
+				else {
+					pos--;
+					mode++;
+					continue;
+				}
+			}
+			else if (mode == 3) {
+				// Specifier
+				fmt.specifier = *pos;
+				//std::cout << "SPECIFIER: " << fmt.specifier << std::endl;
+				pos++;
+				break;
 			}
 		}
-		else if (mode == 3) {
-			// Specifier
-			fmt.specifier = *pos;
-			//std::cout << "SPECIFIER: " << fmt.specifier << std::endl;
-			pos++;
-			break;
+
+		if (!(fmt.specifier == 'd' || fmt.specifier == 'i' || fmt.specifier == 'u' || fmt.specifier == 'o'
+			|| fmt.specifier == 'x' || fmt.specifier == 'X' || fmt.specifier == 'f' || fmt.specifier == 'F'
+			|| fmt.specifier == 'e' || fmt.specifier == 'E' || fmt.specifier == 'g' || fmt.specifier == 'G'
+			|| fmt.specifier == 'a' || fmt.specifier == 'A' || fmt.specifier == 's' || fmt.specifier == 'c' || fmt.specifier == 'p')) {
+
+			if (!(_line_ == -1 || _file_ == nullptr)) {
+				std::cerr << "Line: " << _line_ << " File: '" << _file_ << '\'' << std::endl;
+			}
+			std::cerr << "Undefined format specifier: '" << fmt.specifier << '\'' << std::endl << std::endl;
+			std::exit(EXIT_FAILURE);
 		}
-	}
-	return fmt;
-};
+		return fmt;
+	};
 
-inline bool _checkVal(char specifier, int &val) {
-	return specifier == 'd' || specifier == 'i' || specifier == 'o' || specifier == 'x' || specifier == 'X';
-};
-inline bool _checkVal(char specifier, unsigned int &val) {
-	return specifier == 'u' || specifier == 'o' || specifier == 'x' || specifier == 'X';
-};
-inline bool _checkVal(char specifier, float &val) {
-	return specifier == 'f' || specifier == 'F' || specifier == 'e' || specifier == 'E' || specifier == 'g' || specifier == 'G' || specifier == 'a' || specifier == 'A';
-};
-inline bool _checkVal(char specifier, double &val) {
-	return specifier == 'f' || specifier == 'F' || specifier == 'e' || specifier == 'E' || specifier == 'g' || specifier == 'G' || specifier == 'a' || specifier == 'A';
-};
-inline bool _checkVal(char specifier, long double &val) {
-	return specifier == 'f' || specifier == 'F' || specifier == 'e' || specifier == 'E' || specifier == 'g' || specifier == 'G' || specifier == 'a' || specifier == 'A';
-};
-inline bool _checkVal(char specifier, std::string &val) {
-	return specifier == 's';
-};
+	inline std::string _specString(int &val) {
+		return "d, i, o, x, X";
+	};
+	inline std::string _specString(unsigned int &val) {
+		return "u, o, x, X";
+	};
+	inline std::string _specString(float &val) {
+		return "f, F, e, E, g, G, a, A";
+	};
+	inline std::string _specString(double &val) {
+		return "f, F, e, E, g, G, a, A";
+	};
+	inline std::string _specString(long double &val) {
+		return "f, F, e, E, g, G, a, A";
+	};
+	inline std::string _specString(std::string &val) {
+		return "s";
+	};
+	template<typename T>
+	inline std::string _specString(T &val) {
+		return "s";
+	};
+	inline std::string _specString(char &val) {
+		return "c";
+	};
+	template<typename T>
+	inline std::string _specString(T *&val) {
+		return "p";
+	};
 
-template<class T>
-inline bool _checkVal(char specifier, T &val) {
-	return specifier == 's';
-};
+	inline bool _checkVal(char specifier, int &val) {
+		return specifier == 'd' || specifier == 'i' || specifier == 'o' || specifier == 'x' || specifier == 'X';
+	};
+	inline bool _checkVal(char specifier, unsigned int &val) {
+		return specifier == 'u' || specifier == 'o' || specifier == 'x' || specifier == 'X';
+	};
+	inline bool _checkVal(char specifier, float &val) {
+		return specifier == 'f' || specifier == 'F' || specifier == 'e' || specifier == 'E' || specifier == 'g' || specifier == 'G' || specifier == 'a' || specifier == 'A';
+	};
+	inline bool _checkVal(char specifier, double &val) {
+		return specifier == 'f' || specifier == 'F' || specifier == 'e' || specifier == 'E' || specifier == 'g' || specifier == 'G' || specifier == 'a' || specifier == 'A';
+	};
+	inline bool _checkVal(char specifier, long double &val) {
+		return specifier == 'f' || specifier == 'F' || specifier == 'e' || specifier == 'E' || specifier == 'g' || specifier == 'G' || specifier == 'a' || specifier == 'A';
+	};
+	inline bool _checkVal(char specifier, std::string &val) {
+		return specifier == 's';
+	};
+	template<class T>
+	inline bool _checkVal(char specifier, T &val) {
+		return specifier == 's';
+	};
+	inline bool _checkVal(char specifier, char &val) {
+		return specifier == 'c';
+	};
+	template<typename T>
+	inline bool _checkVal(char specifier, T *&val) {
+		return specifier == 'p';
+	};
 
-inline bool _checkVal(char specifier, char &val) {
-	return specifier == 'c';
-};
-template<typename T>
-inline bool _checkVal(char specifier, T *&val) {
-	return specifier == 'p';
-};
+	template<typename T>
+	inline void _formatVal(const int _line_, const char *_file_, std::ostringstream &ret, std::string::const_iterator &pos, std::string::const_iterator &fmtE, T &val) {
+		/// Modifies pos as it munches the formatting declaration!
+		/// Provides typechecking on formatting declaration because we know the type of val
+		_Format f = _parseFormat(_line_, _file_, pos, fmtE);
 
-template<typename T>
-inline void _formatVal(std::stringstream &ret, std::string::const_iterator &pos, std::string::const_iterator &fmtE, T &val) {
-	/// Modifies pos as it munches the formatting declaration!
-	/// Provides typechecking on formatting declaration because we know the type of val
-	_Format f = _parseFormat(pos, fmtE);
-	
-	assert(_checkVal(f.specifier, val) && "Invalid specifier for type!");
+		if (!_checkVal(f.specifier, val)) {
+			if (!(_line_ == -1 || _file_ == nullptr)) {
+				std::cerr << "Line: " << _line_ << " File: '" << _file_ << '\'' << std::endl;
+			}
+			std::cerr << "Incorrect format specifier: Saw '" << f.specifier << "' | Expected '" << _specString(val) << '\'' << std::endl << std::endl;
+			std::exit(EXIT_FAILURE);
+		}
 
-	std::ios state(NULL);
-	state.copyfmt(ret);
+		std::ios state(NULL);
+		state.copyfmt(ret);
 
-	ret << std::boolalpha;
-	if (f.forceSign) ret << std::showpos;
-	if (f.forceLong) ret << std::showpoint << std::showbase;
-	if (f.padZeros)  ret << std::setfill('0'); 
-	if (f.leftJustify)  ret << std::left;
-	if (f.width > 0) ret << std::setw(f.width);
-	if (f.precision > 0) ret << std::setprecision(f.precision);
+		ret << std::boolalpha;
+		if (f.forceSign) ret << std::showpos;
+		if (f.forceLong) ret << std::showpoint << std::showbase;
+		if (f.padZeros)  ret << std::setfill('0');
+		if (f.leftJustify)  ret << std::left;
+		if (f.width > 0) ret << std::setw(f.width);
+		if (f.precision > 0) ret << std::setprecision(f.precision);
+		if (std::isupper(f.specifier)) ret << std::uppercase;
 
-	switch (f.specifier) {
-	case 'o': 
-		ret << std::oct;
-		if (f.forceLong) ret << 0;
-		break;
-	case 'x':
-	case 'X':
-		ret << std::hex;
-		break;
-	case 'a':
-	case 'A':
-		ret << std::hexfloat;
-		break;
-	case 'e':
-	case 'E':
-		ret << std::scientific;
-		break;
-	case 'p':
-		ret << std::hex;
+		switch (f.specifier) {
+		case 'o':
+			ret << std::oct;
+			if (f.forceLong) ret << '0';
+			break;
+		case 'x':
+		case 'X':
+			ret << std::hex;
+			break;
+		case 'a':
+		case 'A':
+			ret << std::hexfloat;
+			break;
+		case 'e':
+		case 'E':
+			ret << std::scientific;
+			break;
+		case 'p':
+			ret << std::showbase << std::hex;
+			unsigned int *ptr = (unsigned int*) &val;
+			ret << *ptr;
+			ret.copyfmt(state);
+			return;
+		}
 		ret << val;
 		ret.copyfmt(state);
-		return;
-	}
-	ret << val;
-	ret.copyfmt(state);
-};
+	};
 
-inline void _formatVal(std::stringstream &ret, std::string::const_iterator &pos, std::string::const_iterator &fmtE, const char *val) {
-	/// Transparently convert c-string to std::string
-	_formatVal(ret, pos, fmtE, std::string(val));
-};
+	inline void _formatVal(const int _line_, const char *_file_, std::ostringstream &ret, std::string::const_iterator &pos, std::string::const_iterator &fmtE, const char *val) {
+		/// Transparently convert c-string to std::string
+		_formatVal(_line_, _file_, ret, pos, fmtE, std::string(val));
+	};
 
-inline void _format(std::stringstream &ret, std::string::const_iterator &fmtS, std::string::const_iterator &fmtE) {
-	/// No args, print remaining fmt string
-	ret << std::string(fmtS, fmtE);
-};
+	inline void _format(const int _line_, const char *_file_, std::ostringstream &ret, std::string::const_iterator &fmtS, std::string::const_iterator &fmtE) {
+		/// No args, print remaining fmt string
+		ret << std::string(fmtS, fmtE);
+	};
 
-template<typename T, typename ...Args>
-inline void _format(std::stringstream &ret, std::string::const_iterator &fmtS, std::string::const_iterator &fmtE, T &val, Args ...args) {
-	/// Find the next format delimiter
-	auto pos = std::find(fmtS, fmtE, '%');
-	if (pos == fmtE) {
-		/// If no delimiter is found print the rest of the fmt string blindly
-		_format(ret, fmtS, fmtE); //  remaining args... are discarded
-	}
-	else {
-		/// Grab fmt before delimiter
-		ret << std::string(fmtS, pos);
-
-		const auto next = pos + 1;
-		if ((next != fmtE) && (*next == '%')) {
-			ret << '%';
-			pos += 2;
-			/// Formate the remaining string
-			_format(ret, pos, fmtE, val, args...);
-		}	
-		else {
-			/// Format current argument
-			_formatVal(ret, pos, fmtE, val);
-			/// Formate the remaining string
-			_format(ret, pos, fmtE, args...);
+	template<typename T, typename ...Args>
+	inline void _format(const int _line_, const char *_file_, std::ostringstream &ret, std::string::const_iterator &fmtS, std::string::const_iterator &fmtE, T &val, Args ...args) {
+		/// Find the next format delimiter
+		auto pos = std::find(fmtS, fmtE, '%');
+		if (pos == fmtE) {
+			/// If no delimiter is found print the rest of the fmt string blindly
+			_format(_line_, _file_, ret, fmtS, fmtE); //  remaining args... are discarded
 		}
-	}
+		else {
+			/// Grab fmt before delimiter
+			ret << std::string(fmtS, pos);
+
+			const auto next = pos + 1;
+			if ((next != fmtE) && (*next == '%')) {
+				/// Special case for % sign
+				ret << '%';
+				pos += 2;
+				/// Formate the remaining string
+				_format(_line_, _file_, ret, pos, fmtE, val, args...);
+			}
+			else {
+				/// Format current argument
+				_formatVal(_line_, _file_, ret, pos, fmtE, val);
+				/// Formate the remaining string
+				_format(_line_, _file_, ret, pos, fmtE, args...);
+			}
+		}
+	};
+
+	template<typename ...Args>
+	inline std::string format(const int _line_, const char *_file_, const std::string &fmt, Args ...args) {
+		/// This is the public interface to the formatter
+		std::ostringstream ret;
+		string_ext::_format(_line_, _file_, ret, fmt.begin(), fmt.end(), args...);
+		return ret.str();
+	};
+}; // Namespace
+
+namespace std {
+	template<typename ...Args>
+	inline std::string format(const std::string &fmt, Args ...args) {
+		return string_ext::format(-1, nullptr, fmt, args...);
+	};
 };
 
-template<typename ...Args>
-inline std::string format(const std::string &fmt, Args ...args) {
-	/// This is the public interface to the formatter
-	std::stringstream ret;
-	_format(ret, fmt.begin(), fmt.end(), args...);
-	return ret.str();
-};
+#define format_str(...) string_ext::format(__LINE__, __FILE__, __VA_ARGS__)
--- string_ext/test.cpp
+++ string_ext/test.cpp
@@ -1,21 +1,36 @@
-#include <iostream>
 #include "string_ext.h"
 
 struct Test {
 	int a;
 	double d;
-};
 
-std::ostream& operator<<(std::ostream& os, const Test &v) {
-	os << format("{%d, %d}", v.a, v.d);
-	return os;
+	friend std::ostream& operator<<(std::ostream& os, const Test &v) {
+		os << format_str("Test{%i, %f}", v.a, v.d);
+		return os;
+	};
 };
 
 int main() {
-
 	Test obj{5, 3.14};
-	std::cout << format("Hello %s %s\n", obj, "Hello world") << std::endl;
 	
-	std::system("PAUSE");
-	return 0;
+	std::cout << format_str("int:\n"
+		"%d, %i, %u, %#o, %#x, %#X, \n\n"
+		"float:\n"
+		"%f, %F, %e, %E, \n%g, %G, %a, %A, \n\n"
+		"double:\n"
+		"%f, %F, %e, %E, \n%g, %G, %a, %A, \n\n"
+		"long double:\n"
+		"%f, %F, %e, %E, \n%g, %G, %a, %A, \n\n"
+		"string, char, ptr, ostream:\n"
+		"\"%s\", '%c', %p, %s\n",
+		-1, -2, 3u, 4, 12, 15,
+		1.1f, 2.2f, 3.3f, 4.4f, 
+		5.5f, 6.6f, 7.f, 8.f,
+		1.1, 2.2, 3.3, 4.4, 
+		5.5, 6.6, 7., 8.,
+		1.1L, 2.2L, 3.3L, 4.4L, 
+		5.5L, 6.6L, 7.L, 8.L,
+		"Hello world.", '&', &obj, obj) << std::endl;
+	
+	return EXIT_SUCCESS;
 };
